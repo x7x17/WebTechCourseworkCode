b271a5717abbead2e61a011026e2f113827a1e6c
x7x17 <x7x17@students.keele.ac.uk>
2023-05-02 13:32:21 +0100

    Update dbh.inc.php
    reverted the db credentials back to local host dets

--- Web Tech Coursework/includes/dbh.inc.php
+++ Web Tech Coursework/includes/dbh.inc.php
@@ -4,20 +4,20 @@
 
 //connections to be used for Local Host XAMMP server
 
-//$serverName = "localhost";
+$serverName = "localhost";
 
-//$dbUsername = "root";
+$dbUsername = "root";
 
-//$dbPassword = "";
+$dbPassword = "";
 
-//$dbName = "twitcherdb";
+$dbName = "twitcherdb";
 
 //connections to be used for Keele W/:drive server
 
-$serverName = "katara.scam.keele.ac.uk";
-$dbUsername = "x7x17";
-$dbPassword = "x7x17x7x17";
-$dbName = "x7x17";
+//$serverName = "katara.scam.keele.ac.uk";
+//$dbUsername = "x7x17";
+//$dbPassword = "x7x17x7x17";
+//$dbName = "x7x17";
 
 $conn = mysqli_connect($serverName, $dbUsername, $dbPassword, $dbName);
 
